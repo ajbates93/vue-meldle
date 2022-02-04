98dc0084b8c35cf288ed1cdcb2c01690f1cede3e
Alex Bates <alex@hciyork.com>
2022-02-04 12:52:35 +0000

    update

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -1,6 +1,7 @@
+import {execa} from 'execa'
+import fs from 'fs'
+
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
