47a7e43221ef90baf207212e24e0b72d0c10350d
Alex Bates <alex@hciyork.com>
2022-02-20 13:35:18 +0000

    update min-width

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="overlay" class="z-40 bg-gray-400 fixed w-full h-full top-0 left-0 transition-all" :class="store.state.settings.showSettings ? 'opacity-75 visible' : 'opacity-0 hidden'"></div>
-  <div class="z-50 w-2/4 fixed p-5 top-2/4 left-2/4 bg-white shadow-xl rounded-lg transition-all" style="transform: translate(-50%, -50%); max-width: 350px;" :class="store.state.settings.showSettings ? 'opacity-100 visible' : 'opacity-0 hidden'">
+  <div class="z-50 w-2/4 fixed p-5 top-2/4 left-2/4 bg-white shadow-xl rounded-lg transition-all" style="transform: translate(-50%, -50%); max-width: 350px; min-width: 300px;" :class="store.state.settings.showSettings ? 'opacity-100 visible' : 'opacity-0 hidden'">
     <div class="relative bg-white">
       <h3 class="text-center font-bold text-xl mb-5">Statistics</h3>
       <a @click="close" class="absolute right-0 top-0" href="javascript:void(0)">
